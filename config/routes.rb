271b0e7077662960b28b5c3a38ea0fe7913e47f0
maruo0501 <sagiri.maruo@gmail.com>
2020-09-22 20:49:44 +0900

    Add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'homes#top'
+  # get 'homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
