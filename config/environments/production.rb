80f8b779509de56cec64d0d0e0b70622509a2f0b
maruo0501 <sagiri.maruo@gmail.com>
2020-09-23 13:59:01 +0900

    Set up heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,10 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # heroku設定時にtrueに変更
+  config.assets.compile = true
+  # heroku設定時に追記
+  config.assets.initialize_on_precompile=false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/new'
+  get 'posts/edit'
   devise_for :users
   root 'homes#top'
   # get 'homes/top'
