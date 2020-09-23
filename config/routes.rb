5398f96bd30d5f96a75fb5218067e5c8d0ad0544
maruo0501 <sagiri.maruo@gmail.com>
2020-09-23 20:10:28 +0900

    Add bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   root 'homes#top'
-  get '/posts/index', to:　'posts#index'
-  get '/posts/:id', to:　'posts#show'
-  get '/posts/:new', to:　'posts#new'
-  get '/posts/:id/edit', to:　'posts#edit'
-  post '/posts/create', to: 'posts#create'
-  post '/posts/:id/update', to: 'posts#update'
-  post '/posts/:id/destroy', to: 'posts#destroy'
+  # get 'posts/index' => 'posts#index'
+  # get 'posts/:id' =>　'posts#show'
+  # get 'posts/:new' => 'posts#new'
+  # get 'posts/:id/edit' => 'posts#edit'
+  # post 'posts/create' => 'posts#create'
+  # post 'posts/:id/update' => 'posts#update'
+  # post 'posts/:id/destroy' => 'posts#destroy'
   devise_for :users
   # get 'homes/top'
 end
