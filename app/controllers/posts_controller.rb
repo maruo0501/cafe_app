f36a0a9baf5cade5c92e7b96591273599e1b9ded
maruo0501 <sagiri.maruo@gmail.com>
2020-09-26 16:08:56 +0900

    Add destroy action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -42,5 +42,9 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    flash[:notice] = "投稿を削除しました"
+    redirect_to("/posts/index")
   end
 end
