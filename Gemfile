7230119ee0bc123eeb6982a6d3fa8e0ece58ac2f
maruo0501 <sagiri.maruo@gmail.com>
2020-09-29 12:55:47 +0900

    Add image

--- Gemfile
+++ Gemfile
@@ -36,6 +36,9 @@ gem 'devise'
 # エラーメッセージの日本語化
 gem 'rails-i18n', '~> 6.0.0'
 
+# 画像アップロード
+gem 'carrierwave'
+gem 'mini_magick'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   root 'homes#top'
+  # resources :posts
+  # resources :posts, only: [:index, :new, :show, :create, :edit, :update]
   get "posts/index" => "posts#index"
   get "posts/new" => "posts#new"
   get "posts/:id" => "posts#show"
   post "posts/create" => "posts#create"
   get "posts/:id/edit" => "posts#edit"
-  post "posts/:id/update" => "posts#update"
+  # post "posts/:id/update" => "posts#update"
   post "posts/:id/destroy" => "posts#destroy"
   devise_for :users
   # get 'homes/top'
+  patch "posts/:id/update" => "posts#update"
 end
