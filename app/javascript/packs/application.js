9d5f7c01c5c68e98faa8d3cf4e5d8531d95d889b
maruo0501 <sagiri.maruo@gmail.com>
2020-10-01 21:55:14 +0900

    Add font awesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,8 @@ require("channels")
 // require("jquery")
 // import 'bootstrap'
 // import '../src/application.scss'
+// 追記
+import '@fortawesome/fontawesome-free/js/all';
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
