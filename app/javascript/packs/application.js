b4c0dc25b60ff01812bd283ec3936cb856cbb164
maruo0501 <sagiri.maruo@gmail.com>
2020-10-02 13:43:36 +0900

    エラー修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,8 +13,6 @@ require("channels")
 // import '../src/application.scss'
 // font awesome読み込み
 import '@fortawesome/fontawesome-free/js/all';
-// import '../css/application.scss'
-// import '../../assets/stylesheets/application.scss'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
