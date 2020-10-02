1a8c1a821bbe8aee6849131a466340659073631d
maruo0501 <sagiri.maruo@gmail.com>
2020-10-02 11:32:42 +0900

    Add font awesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,7 +13,8 @@ require("channels")
 // import '../src/application.scss'
 // 追記
 import '@fortawesome/fontawesome-free/js/all';
-import '../css/application.scss'
+// import '../css/application.scss'
+import '../../assets/stylesheets/application.scss'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
