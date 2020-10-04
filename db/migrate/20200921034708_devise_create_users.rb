d86ce84f374eac0f500a5c58742fd6211928f3b0
Sagiri Maruo <sagiri.maruo@gmail.com>
2020-10-04 14:48:54 +0900

    エラー修正

--- db/migrate/20200921034708_devise_create_users.rb
+++ db/migrate/20200921034708_devise_create_users.rb
@@ -9,7 +9,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
 
       # 追加
       t.string :name
-      t.string :image_name
+      t.string :picture
 
       ## Recoverable
       t.string   :reset_password_token
