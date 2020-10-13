4867b8b66eb2aae35b9a6227282d34f140572e2f
Sagiri Maruo <sagiri.maruo@gmail.com>
2020-10-13 22:08:11 +0900

    エラー修正

--- db/migrate/20200923052836_create_posts.rb
+++ db/migrate/20200923052836_create_posts.rb
@@ -2,17 +2,8 @@ class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
       t.text :content
-      t.string :picture
       t.string :store_name
-      # t.integer :user_id
-
-      # 追記
-      # t.integer :wifi, default: 0, null: false
-      # t.integer :power, default: 0, null: false
-      # t.integer :creditcard, default: 0, null: false
-
       t.references :user, foreign_key: true
-
       t.timestamps
     end
   end
