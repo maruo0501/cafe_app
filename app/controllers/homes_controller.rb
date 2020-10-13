76a3816b66ab7e3e79f9a563d4a433b182d72899
Sagiri Maruo <sagiri.maruo@gmail.com>
2020-10-13 16:04:30 +0900

    Add post_index

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,10 @@
 class HomesController < ApplicationController
+  before_action :post_index, only: [:top]
   def top
-    
+  end
+
+  def post_index
+    @posts = Post.page(params[:page]).per(6).order(created_at: :desc)
   end
 end
 
