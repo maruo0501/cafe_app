f9e24f5b2480ad4ccbf91c49b318b2ed6475bb78
Sagiri Maruo <sagiri.maruo@gmail.com>
2020-10-14 17:30:11 +0900

    Add ajax

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,18 @@
 class FavoritesController < ApplicationController
+  before_action :set_post
+  
   def create
     favorite = current_user.favorites.build(post_id: params[:post_id])
-    if favorite.save!
-      redirect_to posts_path
-    else
-      flash.now[:danger] = "お気に入りに失敗しました"
-      redirect_to posts_path
-    end
+    favorite.save
   end
 
   def destroy
     favorite = Favorite.find_by(post_id: params[:post_id], user_id: current_user.id)
     favorite.destroy
-    redirect_to posts_path
+  end
+
+  private
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 end
