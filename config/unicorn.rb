4d51d4a6842d3ea61e87b6a8966dbc72dedf305f
Sagiri Maruo <sagiri.maruo@gmail.com>
2020-11-05 20:47:52 +0900

    Fix code

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -10,7 +10,8 @@ working_directory app_path
 pid "#{app_path}/tmp/pids/unicorn.pid"
 
 #ポート番号を指定
-listen "#{app_path}/tmp/sockets/unicorn.sock"
+listen 3000
+# "#{app_path}/tmp/sockets/unicorn.sock"
 
 #エラーのログを記録するファイルを指定
 stderr_path "#{app_path}/log/unicorn.stderr.log"
