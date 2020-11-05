00a8ddc70009dc263727193636540cac8d6ee1b5
Sagiri Maruo <sagiri.maruo@gmail.com>
2020-11-05 12:17:29 +0900

    Fix code

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,7 +11,8 @@ unless Rails.env.development? || Rails.env.test?
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: ENV['AWS_DEFAULT_REGION'],
+      region: 'ap-northeast-1'
+      # ENV['AWS_DEFAULT_REGION'],
       path_style: true
     }
   end
