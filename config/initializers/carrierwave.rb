4c412ec834869cc552b209790305be7203ac5e42
Sagiri Maruo <sagiri.maruo@gmail.com>
2020-11-05 13:59:13 +0900

    AWS

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,7 +12,6 @@ unless Rails.env.development? || Rails.env.test?
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region: 'ap-northeast-1',
-      # ENV['AWS_DEFAULT_REGION'],
       path_style: true
     }
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/test-cafeapp'
