e8f25a7b3e2edefce13d3e8cfe731b664cfb9076
Sagiri Maruo <sagiri.maruo@gmail.com>
2020-12-22 18:14:49 +0900

    Fix rspec

--- spec/controllers/favorites_controller_spec.rb
+++ spec/controllers/favorites_controller_spec.rb
@@ -1,45 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe FavoritesController, :type => :controller do
+  let(:user) { create(:user) }
+  let(:new_post) { create(:post) }
   describe "POST #create" do
     # ログイン済みのユーザーとして
     context "as an authenticated user" do
-      let!(:user) { create(:user) }
-      let!(:new_post) { create(:post) }
       # いいね！が出来ること
       it "creates a favorite" do
         sign_in user
-        # expect { post :create, :params => { :user_id => user.id, :post_id => post.id } }.to change(user.favorites, :count).by(1)
-        # expect (post.save).to change(user.favorites, :count).by(1)
-        # expect {
-        #   post :create, params: { post: new_post }
-        # }.to change(user.favorites, :count).by(1)
+        expect { post :create, :params => { :user_id => user.id, :post_id => new_post.id } }.to change(user.favorites, :count).by(1)
       end
     end
     # ログインしてないゲストユーザーとして
     context "as an guest user" do
-      before do
-        @user = create(:user)
-        @post = create(:post)
-      end
       # いいね！しようとするとエラーが発生すること
       it "does not create favorite for a post and gets an error" do
-        expect { post :create, :params => { :user_id => @user.id, :post_id => @post.id } }.to raise_error NameError
+        expect { post :create, :params => { :user_id => user.id, :post_id => new_post.id } }.to raise_error NameError
       end
     end
   end
   describe "DELETE #destroy" do
     # ログイン済みのユーザーとして
     context "as an authenticated user" do
-      before do
-        @user = create(:user)
-        @post = create(:post)
-      end
       # いいね！の取り消しが出来ること
       it "destroys a favorite" do
-        sign_in @user
-        post :create, :params => { :user_id => @user.id, :post_id => @post.id }
-        expect { delete :destroy, :params => { :user_id => @user.id, :post_id => @post.id } }.to change(@user.favorites, :count).by(-1)
+        sign_in user
+        post :create, :params => { :user_id => user.id, :post_id => new_post.id }
+        expect { delete :destroy, :params => { :user_id => user.id, :post_id => new_post.id } }.to change(user.favorites, :count).by(-1)
       end
     end
   end
