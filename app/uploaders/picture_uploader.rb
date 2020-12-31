cb561c3d0acd9a1261bbb12b63eeb49649b3e70b
Sagiri Maruo <sagiri.maruo@gmail.com>
2020-12-31 19:40:16 +0900

    Fix code

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,6 +1,4 @@
 class PictureUploader < CarrierWave::Uploader::Base
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
