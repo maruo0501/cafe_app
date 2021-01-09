aaa32beaed68e57c491b29e4d1e94d672f0f9edf
Sagiri Maruo <sagiri.maruo@gmail.com>
2021-01-09 15:05:30 +0900

    Fix code

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -8,8 +8,6 @@ class PictureUploader < CarrierWave::Uploader::Base
     storage :file
   end
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
