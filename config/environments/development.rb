fb3cc4f422e7efc02185b0f466e2645edeb7f7de
Sagiri Maruo <sagiri.maruo@gmail.com>
2021-01-20 22:33:46 +0900

    Fix code

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,6 +55,5 @@ Rails.application.configure do
   config.assets.quiet = true
 
   # Use an evented file watcher to asynchronously detect changes in source code,
-  # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
