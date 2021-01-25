2876f3cad1268359ff8543afcb3b8b635d7907e9
Sagiri Maruo <sagiri.maruo@gmail.com>
2021-01-25 22:52:00 +0900

    Fix code

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,7 +48,6 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-  # Debug mode disables concatenation and preprocessing of assets.
   config.assets.debug = true
 
   config.assets.quiet = true
